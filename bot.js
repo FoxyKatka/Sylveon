e3590a808fdfa0656fa65960108bc36955576399
Jordan <jordangill600@gmail.com>
2018-08-09 18:54:15 +0100

    Basic

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -7,10 +7,18 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '!trans') {
-        // send back "<3." to the channel the message was sent in
-        message.channel.send('<3');
+    if (message.content === `${prefix}trans`) {
+        message.channel.send('<3');  // send back "<3." to the channel the message was sent in
+    }
+    else if (message.content === `${prefix}beep`) {
+        message.channel.send('Boop.');
+    }
+    else if (message.content === `${prefix}server`) {
+        message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nCreated: ${message.guild.createdAt}\nRegion: ${message.guild.region}`);
+    }
+    else if (message.content === `${prefix}user-info`) {
+        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
     }
 });
 
-client.login(config.token); // no-hack-please
+client.login(token); // no-hack-please
